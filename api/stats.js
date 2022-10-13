f972f8930fe030d379e7b31bcb2750b35427bbea
amarpathak <amar@revise.network>
2022-10-13 17:59:16 +0530

    some

--- api/stats.js
+++ api/stats.js
@@ -7,12 +7,13 @@ const revise = new Revise({ auth: AUTH_TOKEN });
 
 
 export default async function  handler(request, response) {
-    const profileNFT = await revise.fetchNFT("4f20e4c8-d0df-4f01-bac5-1d5f65fe3750")
+    const profileNFT = await revise.fetchNFT("35472372-08d2-4fb2-94c9-3dbcf7f2245c")
     let viewCount = profileNFT.viewCount;
     
     console.log({profileNFT});
     revise.nft(profileNFT)
-    .setProperty('pageViews',viewCount)
+    .setProperty('views',viewCount)
+    .save();
 
 
     response.end('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPjPCgACDQEGvDvlXgAAAABJRU5ErkJggg==')
