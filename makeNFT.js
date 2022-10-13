2c6050374cefee6690a78a5a70c6ce8c17efaac5
amarpathak <amar@revise.network>
2022-10-13 18:03:56 +0530

    fdsf

--- makeNFT.js
+++ makeNFT.js
@@ -6,6 +6,7 @@ const revise = new Revise({ auth: AUTH_TOKEN });
 // const reviseConfig = require('../config/revise.json');
 const collectionId =  '31c2c13c-b5ed-4576-b7b4-ccb9eb50ab47'; //Github
 
+
 const run = async () => {
     // /Create one collection
     // const collection = await revise.addCollection({ name: "Github", uri: 'github' })
@@ -23,17 +24,26 @@ const run = async () => {
 
 
 
-    const nft = await revise.addNFT({
-        image: 'https://revise-testing.fra1.cdn.digitaloceanspaces.com/doggo/happy.png',
-        name: 'Github Profile',
-        tokenId: '1',
-        description: 'Site Stats Github'
-    }, [
-        { views: "10" },
-    ], collectionId)
+    // const nft = await revise.addNFT({
+    //     image: 'https://revise-testing.fra1.cdn.digitaloceanspaces.com/doggo/happy.png',
+    //     name: 'Github Profile',
+    //     tokenId: '1',
+    //     description: 'Site Stats Github'
+    // }, [
+    //     { views: "10" },
+    // ], collectionId)
 
 
-    console.log({ nft })
+    // console.log({ nft })
+
+    const profileNFT = await revise.fetchNFT("35472372-08d2-4fb2-94c9-3dbcf7f2245c")
+    console.log({profileNFT});
+    let viewCount = profileNFT.viewCount;
+    
+    revise.nft(profileNFT)
+    .setProperty('views',20)
+    .save();
+
 
 }
 run();
