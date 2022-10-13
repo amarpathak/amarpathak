67db14881ee887ef1b43b7dadae9d78184d5743f
amarpathak <amar@revise.network>
2022-10-13 17:47:56 +0530

    dfsa

--- api/stats.js
+++ api/stats.js
@@ -1,5 +1,6 @@
-import {Revise} from 'revise-sdk';
-
+const { Revise } = require("revise-sdk");
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI5NTNkYzQ4LTM1MGMtNDYxNC1iYmExLTNiOWU3NDE0MDkwNCIsImtleSI6InY0eDAyOThtIiwiaWF0IjoxNjY1MDMxMjY3fQ.lB08CUwKENDUNxUIoU3HjszaHgrHFZ-OGL-x9W9ykOo";
+const revise = new Revise({ auth: AUTH_TOKEN });
 
 
 // const profileNFT = await revise.fetchNFT("4f20e4c8-d0df-4f01-bac5-1d5f65fe3750")
