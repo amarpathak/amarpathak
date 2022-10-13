7c39db5cfcc567fb5c6d53e8389187bf1785e8b7
amarpathak <amar@revise.network>
2022-10-13 17:42:31 +0530

    Base

--- api/stats.js
+++ api/stats.js
@@ -1,8 +1,18 @@
-export default function handler(request, response) {
-    response.status(200).json({
-      body: request.body,
-      query: request.query,
-      cookies: request.cookies,
-    });
-  }
+import revise from 'revise-sdk';
+
+let viewCount = 9;
+
+const profileNFT = await revise.fetchNFT("4f20e4c8-d0df-4f01-bac5-1d5f65fe3750")
+
+console.log({profileNFT});
+
+export default async function  handler(request, response) {
+   const profileNFT = await revise.fetchNFT("4f20e4c8-d0df-4f01-bac5-1d5f65fe3750")
+
+    revise.nft(profileNFT)
+    .setProperty('pageViews',viewCount)
+
+
+    response.end('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPjPCgACDQEGvDvlXgAAAABJRU5ErkJggg==')
+}
   
---
+++ makeNFT.js
@@ -0,0 +1,39 @@
+const { Revise } = require("revise-sdk");
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI5NTNkYzQ4LTM1MGMtNDYxNC1iYmExLTNiOWU3NDE0MDkwNCIsImtleSI6InY0eDAyOThtIiwiaWF0IjoxNjY1MDMxMjY3fQ.lB08CUwKENDUNxUIoU3HjszaHgrHFZ-OGL-x9W9ykOo";
+const revise = new Revise({ auth: AUTH_TOKEN });
+
+
+// const reviseConfig = require('../config/revise.json');
+const collectionId =  '31c2c13c-b5ed-4576-b7b4-ccb9eb50ab47'; //Github
+
+const run = async () => {
+    // /Create one collection
+    // const collection = await revise.addCollection({ name: "Github", uri: 'github' })
+    // console.log({ collection });
+    
+
+    // //Get All Cllections
+    // try {
+    //     const collections = await revise.fetchCollections();
+    //     console.log({ collections });
+
+    // } catch (error) {
+
+    // }
+
+
+
+    const nft = await revise.addNFT({
+        image: 'https://revise-testing.fra1.cdn.digitaloceanspaces.com/doggo/happy.png',
+        name: 'Github Profile',
+        tokenId: '1',
+        description: 'Site Stats Github'
+    }, [
+        { views: "10" },
+    ], collectionId)
+
+
+    console.log({ nft })
+
+}
+run();
